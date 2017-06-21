3ba82c49b27361255df1d6ed05d319d80470168e
Tomo Bessho <tzxbApp018@gmail.com>
2017-06-21 12:42:06 -0500

    First FlightInfo Draft

--- ARA/ARA.Android/MainActivity.cs
+++ ARA/ARA.Android/MainActivity.cs
@@ -19,6 +19,8 @@ namespace ARA.Droid
 
 			base.OnCreate (bundle);
 
+            SetContentView(Resource.Layout.FlightInfo1);
+
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new ARA.App ());
 		}

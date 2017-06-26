6916c2429925781e4a8b2f9485ff2eca87581a6c
Tomo Bessho <tzxbApp018@gmail.com>
2017-06-26 10:39:51 -0500

    VFR and IFR Times page working

--- ARA/ARA.Android/C_Time_IFR_Activity.cs
+++ ARA/ARA.Android/C_Time_IFR_Activity.cs
@@ -15,6 +15,9 @@ namespace ARA.Droid
     [Activity(Label = "Type of Flight - 3 of 5")]
     public class C_Time_IFR_Activity : Activity
     {
+        public static bool IFRisDual;
+        public static bool IFRisDay;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,6 +27,48 @@ namespace ARA.Droid
 
             var back = FindViewById<ImageButton>(Resource.Id.btnBackfromIFRTime);
             var next = FindViewById<ImageButton>(Resource.Id.btnContinuefromIFRTime);
+            var IFR_Dual = FindViewById<Button>(Resource.Id.btnDualIFR);
+            var IFR_Day = FindViewById<Button>(Resource.Id.btnDayIFR);
+            var IFR_Text = FindViewById<TextView>(Resource.Id.txtIFRTime);
+
+            if (IFRisDual) //opens the page with the options already selected
+            {
+                IFR_Dual.Selected = true;
+                IFR_Day.Selected = false;
+                IFR_Day.Pressed = true; //changing the states of the two buttons to allow the user to see which he/she has selected
+                IFR_Dual.RequestFocus();
+                IFR_Text.Text = "You have selected the Day/Night Option.";
+            }
+            else if (IFRisDay)
+            {
+                IFR_Day.Selected = true;
+                IFR_Dual.Selected = false;
+                IFR_Dual.Pressed = true;
+                IFR_Day.RequestFocus();
+                IFR_Text.Text = "You have selcted the Day Time Option.";
+            }
+
+            IFR_Dual.Click += delegate
+            {
+                IFR_Dual.Selected = true;
+                IFR_Day.Selected = false;
+                IFR_Day.Pressed = true; //changing the states of the two buttons to allow the user to see which he/she has selected
+                IFR_Dual.RequestFocus();
+                IFRisDual = true;
+                IFRisDay = false; //changing the variables
+                IFR_Text.Text = "You have selected the Day/Night Option.";
+            };
+
+            IFR_Day.Click += delegate
+            {
+                IFR_Day.Selected = true;
+                IFR_Dual.Selected = false;
+                IFR_Dual.Pressed = true;
+                IFR_Day.RequestFocus();
+                IFRisDay = true;
+                IFRisDual = false;
+                IFR_Text.Text = "You have selcted the Day Time Option.";
+            };
 
             back.Click += delegate
             {
--- ARA/ARA.Android/C_Time_VFR_Activity.cs
+++ ARA/ARA.Android/C_Time_VFR_Activity.cs
@@ -15,6 +15,10 @@ namespace ARA.Droid
     [Activity(Label = "Type of Flight - 3 of 5")]
     public class C_Time_VFR_Activity : Activity
     {
+        public static bool VFRisDay;
+        public static bool VFRisDual;
+        public static bool VFRisNight;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,6 +28,71 @@ namespace ARA.Droid
 
             var back = FindViewById<ImageButton>(Resource.Id.btnBackFromVFRTime);
             var next = FindViewById<ImageButton>(Resource.Id.btnContinuefromVFRTime);
+            var VFR_Day = FindViewById<Button>(Resource.Id.btnDayVFR);
+            var VFR_Dual = FindViewById<Button>(Resource.Id.btnDualVFR);
+            var VFR_Night = FindViewById<Button>(Resource.Id.btnNightVFR);
+            var VFR_Text = FindViewById<TextView>(Resource.Id.txtVFRTime);
+            
+            //setting up the screen using the boolean values (if first time, all bool will be false, therefore none of these will run)
+            if (VFRisDay)
+            {
+                VFR_Day.Selected = true;
+                VFR_Dual.Selected = false;
+                VFR_Night.Selected = false;
+                VFR_Day.RequestFocus();
+                VFR_Text.Text = "You have selected the Day Option.";
+            }
+            if (VFRisDual)
+            {
+                VFR_Dual.Selected = true;
+                VFR_Day.Selected = false;
+                VFR_Night.Selected = false;
+                VFR_Dual.RequestFocus();
+                VFR_Text.Text = "You have selected the Day/Night Option.";
+            }
+            if (VFRisNight)
+            {
+                VFR_Night.Selected = true;
+                VFR_Day.Selected = false;
+                VFR_Dual.Selected = false;
+                VFR_Night.RequestFocus();
+                VFR_Text.Text = "You have selected the Night Option.";
+            }
+
+            //when one of them are clicked, the rest are unselected, because only one option can be selected
+            VFR_Day.Click += delegate
+            {
+                VFR_Day.Selected = true;
+                VFR_Dual.Selected = false;
+                VFR_Night.Selected = false;
+                VFR_Day.RequestFocus();
+                VFR_Text.Text = "You have selected the Day Option."; //displaying the selection
+                VFRisDay = true;
+                VFRisDual = false;
+                VFRisNight = false; //setting and changing the respecitive booleans
+            };
+            VFR_Dual.Click += delegate
+            {
+                VFR_Day.Selected = false;
+                VFR_Dual.Selected = true;
+                VFR_Night.Selected = false;
+                VFR_Dual.RequestFocus();
+                VFR_Text.Text = "You have selected the Day/Night Option."; //displaying the selection
+                VFRisDay = false;
+                VFRisDual = true;
+                VFRisNight = false; //setting and changing the respecitive booleans
+            };
+            VFR_Night.Click += delegate
+            {
+                VFR_Day.Selected = false;
+                VFR_Dual.Selected = false;
+                VFR_Night.Selected = true;
+                VFR_Night.RequestFocus();
+                VFR_Text.Text = "You have selected the Night Option."; //displaying the selection
+                VFRisDay = false;
+                VFRisDual = false;
+                VFRisNight = true; //setting and changing the respecitive booleans
+            };
 
             back.Click += delegate
             {

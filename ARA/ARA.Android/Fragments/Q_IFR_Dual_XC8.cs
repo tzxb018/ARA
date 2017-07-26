be66594987baa9f16d28ad94cabd594414870002
Tomo Bessho <tzxbApp018@gmail.com>
2017-07-26 12:35:44 -0500

    alt programmed

---
+++ ARA/ARA.Android/Fragments/Q_IFR_Dual_XC8.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ARA.Droid.Fragments
+{
+    public class Q_IFR_Dual_XC8 : Android.Support.V4.App.Fragment
+    {
+        private ImageButton btnNext;
+        private ImageButton btnBack;
+        private TextView q1;
+        private TextView q2;
+        private TextView q3;
+        private Button ans11;
+        private Button ans12;
+        private Button ans13;
+        private Button ans21;
+        private Button ans22;
+        private Button ans23;
+        private Button ans31;
+        private Button ans32;
+        private Button ans33;
+        private TextView ans1;
+        private TextView ans2;
+        private TextView ans3;
+
+        public static int risk3, risk4, risk5;
+
+        private OnFragmentInteractionListener mListener;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.LayoutFragment3, container, false);
+
+            q1 = view.FindViewById<TextView>(Resource.Id.txtQuestion1);
+            ans11 = view.FindViewById<Button>(Resource.Id.btnQ1C1);
+            ans12 = view.FindViewById<Button>(Resource.Id.btnQ1C2);
+            ans13 = view.FindViewById<Button>(Resource.Id.btnQ1C3);
+            ans1 = view.FindViewById<TextView>(Resource.Id.txtAnswer1);
+
+            q2 = view.FindViewById<TextView>(Resource.Id.txtQuestion2);
+            ans21 = view.FindViewById<Button>(Resource.Id.btnQ2C1);
+            ans22 = view.FindViewById<Button>(Resource.Id.btnQ2C2);
+            ans23 = view.FindViewById<Button>(Resource.Id.btnQ2C3);
+            ans2 = view.FindViewById<TextView>(Resource.Id.txtAnswer2);
+
+            q3 = view.FindViewById<TextView>(Resource.Id.txtQuestion3);
+            ans31 = view.FindViewById<Button>(Resource.Id.btnQ3C1);
+            ans32 = view.FindViewById<Button>(Resource.Id.btnQ3C2);
+            ans33 = view.FindViewById<Button>(Resource.Id.btnQ3C3);
+            ans3 = view.FindViewById<TextView>(Resource.Id.txtAnswer3);
+
+            btnBack = view.FindViewById<ImageButton>(Resource.Id.btnBackfrom3);
+            btnNext = view.FindViewById<ImageButton>(Resource.Id.btnContinueFrom3);
+
+            ShortCutFunctions sc = new ShortCutFunctions();
+
+            
+
+            /*
+            q1.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum][0];
+            ans11.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum][1];
+            ans12.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum][2];
+            ans13.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum][3];
+
+            q2.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 1][0];
+            ans21.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 1][1];
+            ans22.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 1][2];
+            ans23.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 1][3];
+
+            q3.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 2][0];
+            ans31.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 2][1];
+            ans32.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 2][2];
+            ans33.Text = result.IFR_Day_Local_Questions_Alternate[N_IFR_Day_Local_Alternate.questionNum + 2][3]; */
+
+            q1.Text = "Wind";
+            ans11.Text = "0 - 15 kts";
+            ans12.Text = "16 - 20 kts";
+            ans13.Text = "21 - 25 kts";
+
+            q2.Text = "Xwind";
+            ans21.Text = "0 - 5 kts";
+            ans22.Text = "6 - 10 kts";
+            ans23.Text = "11 - 15 kts";
+
+            q3.Text = "Fuel Remaining";
+            ans31.Text = "> 75 min";
+            ans32.Text = "60 - 75 min";
+            ans33.Text = "45 - 60 min";
+
+            return view;
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+
+            ShortCutFunctions sc = new ShortCutFunctions();
+
+            sc.defaultVals(ans11, ans12, ans13, ans1, Q_IFR_Dual_XC_8Alternate.questionArray[Q_IFR_Dual_XC_8Alternate.questionNum]);
+            sc.defaultVals(ans21, ans22, ans23, ans2, Q_IFR_Dual_XC_8Alternate.questionArray[Q_IFR_Dual_XC_8Alternate.questionNum + 1]);
+            sc.defaultVals(ans31, ans32, ans33, ans3, Q_IFR_Dual_XC_8Alternate.questionArray[Q_IFR_Dual_XC_8Alternate.questionNum + 2]);
+
+            ans11.Touch += (s, e) =>
+            {
+                risk3 = 0;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button1Pressed(ans11, ans12, ans13, ans1);
+            };
+            ans12.Touch += (s, e) =>
+            {
+                risk3 = 1;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button2Pressed(ans11, ans12, ans13, ans1);
+            };
+            ans13.Touch += (s, e) =>
+            {
+                risk3 = 3;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button3Pressed(ans11, ans12, ans13, ans1);
+            };
+
+            ans21.Touch += (s, e) =>
+            {
+                risk4 = 0;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button1Pressed(ans21, ans22, ans23, ans2);
+            };
+            ans22.Touch += (s, e) =>
+            {
+                risk4 = 1;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button2Pressed(ans21, ans22, ans23, ans2);
+            };
+            ans23.Touch += (s, e) =>
+            {
+                risk4 = 3;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button3Pressed(ans21, ans22, ans23, ans2);
+            };
+
+            ans31.Touch += (s, e) =>
+            {
+                risk3 = 0;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button1Pressed(ans31, ans32, ans33, ans3);
+            };
+            ans32.Touch += (s, e) =>
+            {
+                risk3 = 1;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button2Pressed(ans31, ans32, ans33, ans3);
+            };
+            ans33.Touch += (s, e) =>
+            {
+                risk3 = 3;
+                mListener.onFragmentInteraction(risk3, risk4, risk3);
+                sc.button3Pressed(ans31, ans32, ans33, ans3);
+            };
+
+
+        }
+
+        public override void OnAttach(Context context)
+        {
+            base.OnAttach(context);
+
+            try
+            {
+                mListener = (OnFragmentInteractionListener)Activity;
+            }
+            catch (Exception ex) { }
+        }
+
+
+        public void onFragmentInteraction(int riskOut, int riskOut2)
+        {
+
+        }
+
+        public void onFragmentInteraction(int riskOut, int riskOut2, int r3)
+        {
+        }
+    }
+}
--- ARA/ARA.Android/Q_IFR_Dual_XC_8Alternate.cs
+++ ARA/ARA.Android/Q_IFR_Dual_XC_8Alternate.cs
@@ -18,17 +18,17 @@ using ARA.Droid.Fragments;
 
 namespace ARA.Droid
 {
-    [Activity(Label = "Destination Airfield", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
-    public class Q_IFR_Dual_XC_6Destination : FragmentActivity, OnFragmentInteractionListener
+    [Activity(Label = "Alternate", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class Q_IFR_Dual_XC_8Alternate : FragmentActivity, OnFragmentInteractionListener
     {
-        public static int destinationRisk;
+        public static int altRisk;
         public static int questionNum;
         private Stack<SupportFragment> mStackFragment;
         private ImageButton btnNext;
         private ImageButton btnBack;
         private SupportFragment mCurrent;
 
-        public static int[] questionArray = new int[4];
+        public static int[] questionArray = new int[5];
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -39,15 +39,15 @@ namespace ARA.Droid
             btnNext = FindViewById<ImageButton>(Resource.Id.btnContinueFragment);
             btnBack = FindViewById<ImageButton>(Resource.Id.btnBackFragment);
 
-            var mFrg1 = new Q_IFR_Dual_XC5();
-            var mFrg2 = new Q_IFR_Dual_XC6();
+            var mFrg1 = new Q_IFR_Dual_XC7();
+            var mFrg2 = new Q_IFR_Dual_XC8();
             mStackFragment = new Stack<SupportFragment>();
 
             var trans = SupportFragmentManager.BeginTransaction();
-            if (questionNum == 4)
+            if (questionNum == 5)
             {
                 mCurrent = mFrg2;
-                questionNum = 2;
+                questionNum = 3;
             }
             else
             {
@@ -67,25 +67,25 @@ namespace ARA.Droid
             var txtRiskNum = FindViewById<TextView>(Resource.Id.txtRiskNumFragment);
 
             ShortCutFunctions sc = new ShortCutFunctions();
-            sc.riskShow(txtRisk, txtRiskNum, "Destination Risk", destinationRisk, 8, 10);
+            sc.riskShow(txtRisk, txtRiskNum, "Alternate Risk", altRisk, 9, 12);
 
             btnNext.Click += (s, e) =>
             {
                 if (mCurrent.Equals(mFrg2)) //determining question
                 {
-                    if (destinationRisk > 9)
+                    if (altRisk > 11)
                     {
-                        sc.alertShow("Destination Risk", this);
+                        sc.alertShow("Alternate Risk", this);
                     }
                     else
                     {
                         StartActivity(typeof(Q_IFR_Dual_XC_7AlternateDayandNight));
-                        questionNum = 4;
+                        questionNum = 5;
                     }
                 }
                 else
                 {
-                    questionNum += 2;
+                    questionNum += 3;
                     replaceFragment(mFrg2);
                 }
             };
@@ -94,12 +94,12 @@ namespace ARA.Droid
             {
                 if (mCurrent.Equals(mFrg1))
                 {
-                    StartActivity(typeof(Q_IFR_Dual_XC_5DestinationDayandNight));
+                    StartActivity(typeof(Y_Aircraft_and_Instructor));
                 }
                 else
                 {
                     replaceFragment(mFrg1);
-                    questionNum -= 2;
+                    questionNum -= 3;
                 }
             };
         }
@@ -116,10 +116,22 @@ namespace ARA.Droid
 
         public void onFragmentInteraction(int riskOut, int riskOut2, int r3)
         {
+            ShortCutFunctions sc = new ShortCutFunctions();
+            try
+            {
+                var txtRisk = FindViewById<TextView>(Resource.Id.txtRiskFragment);
+                var txtRiskNum = FindViewById<TextView>(Resource.Id.txtRiskNumFragment);
+                altRisk = Q_IFR_Dual_XC_7AlternateDayandNight.ceilingRisk + Q_IFR_Dual_XC7.risk1 + Q_IFR_Dual_XC7.risk2 + Q_IFR_Dual_XC8.risk3 + Q_IFR_Dual_XC8.risk4 + Q_IFR_Dual_XC8.risk5;
+                sc.riskShow(txtRisk, txtRiskNum, "Enroute Risk", altRisk, 9, 12);
+                questionArray[questionNum] = riskOut;
+                questionArray[questionNum + 1] = riskOut2;
+                questionArray[questionNum + 2] = r3;
+            }
+            catch (Exception e)
+            {
+                throw new NotImplementedException();
 
-            throw new NotImplementedException();
-
-
+            }
         }
 
         public void onFragmentInteraction(int riskOut, int riskOut2)
@@ -129,8 +141,8 @@ namespace ARA.Droid
             {
                 var txtRisk = FindViewById<TextView>(Resource.Id.txtRiskFragment);
                 var txtRiskNum = FindViewById<TextView>(Resource.Id.txtRiskNumFragment);
-                destinationRisk = Q_IFR_Dual_XC5.risk1 + Q_IFR_Dual_XC5.risk2 + Q_IFR_Dual_XC6.risk3 + Q_IFR_Dual_XC6.risk4 + Q_IFR_Dual_XC_5DestinationDayandNight.ceilingRisk;
-                sc.riskShow(txtRisk, txtRiskNum, "Destination Risk", destinationRisk, 8, 10);
+                altRisk = Q_IFR_Dual_XC_7AlternateDayandNight.ceilingRisk + Q_IFR_Dual_XC7.risk1 + Q_IFR_Dual_XC7.risk2 + Q_IFR_Dual_XC8.risk3 + Q_IFR_Dual_XC8.risk4 + Q_IFR_Dual_XC8.risk5;
+                sc.riskShow(txtRisk, txtRiskNum, "Enroute Risk", altRisk, 9, 12);
                 questionArray[questionNum] = riskOut;
                 questionArray[questionNum + 1] = riskOut2;
             }
